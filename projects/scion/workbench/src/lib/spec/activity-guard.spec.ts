86b77f1cc1e4077b55091b79ba1434499cc472a4
Daniel Wiehl <daniel.wiehl@yahoo.com>
2018-07-29 20:30:25 +0200

    fix: Render correct actions in the activity part header
    Activity part did not render the correct actions in the activity part header when toggling activities.
    
    Fixes #9

---
+++ projects/scion/workbench/src/lib/spec/activity-guard.spec.ts
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2018 Swiss Federal Railways
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ *  SPDX-License-Identifier: EPL-2.0
+ */
+
+import { async, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
+import { Component, Injectable, NgModule } from '@angular/core';
+import { WorkbenchModule } from '../workbench.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouteReuseStrategy, RouterStateSnapshot } from '@angular/router';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { advance, clickElement } from './util/util.spec';
+import { expect, jasmineCustomMatchers } from './util/jasmine-custom-matchers.spec';
+import { ActivityPartComponent } from '../activity-part/activity-part.component';
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs/index';
+
+/**
+ *
+ * Testsetup:
+ *
+ * +--------------------------------------------+
+ * | Test Module                                |
+ * |--------------------------------------------|
+ * | routes:                                    |
+ * |                                            |
+ * | 'activity-1' => Activity1Component         |
+ * | 'activity-2' => Activity2Component (guard) |
+ * | 'activity-3' => Activity3Component         |
+ * +--------------------------------------------+
+ *
+ */
+describe('Activity part', () => {
+
+  beforeEach(async(() => {
+    jasmine.addMatchers(jasmineCustomMatchers);
+
+    TestBed.configureTestingModule({
+      imports: [AppTestModule, CommonModule]
+    });
+
+    TestBed.get(Router).initialNavigation();
+  }));
+
+  it('does not activate activity if prevented by routing guard', fakeAsync(inject([Router, RouteReuseStrategy], (router: Router, routeReuseStrategy: RouteReuseStrategy) => {
+    // Setup: set workbench reuse strategy
+    router.routeReuseStrategy = routeReuseStrategy;
+
+    const fixture = TestBed.createComponent(AppComponent);
+    advance(fixture);
+
+    // Open 'activity-1'
+    clickElement(fixture, ActivityPartComponent, 'a.activity-1', '(1a)');
+    expect(fixture).toShow(Activity1Component, '(1b)');
+    expect(fixture.debugElement.query(By.css('header > h1')).nativeElement.textContent).toEqual('Activity 1', '(1c)');
+
+    // Try open 'activity-2' (guard prevents activation)
+    Activity2CanActivate.CAN_ACTIVATE = false;
+    clickElement(fixture, ActivityPartComponent, 'a.activity-2', '(2a)');
+    expect(fixture).toShow(Activity1Component, '(2b)'); // {Activity1Component} should still be showing
+    expect(fixture.debugElement.query(By.css('header > h1')).nativeElement.textContent).toEqual('Activity 1', '(2c)');
+
+    // Open 'activity-3'
+    clickElement(fixture, ActivityPartComponent, 'a.activity-3', '(3a)');
+    expect(fixture).toShow(Activity3Component, '(3b)');
+    expect(fixture.debugElement.query(By.css('header > h1')).nativeElement.textContent).toEqual('Activity 3', '(3c)');
+
+    // Try open 'activity-2' (guard prevents activation)
+    Activity2CanActivate.CAN_ACTIVATE = false;
+    clickElement(fixture, ActivityPartComponent, 'a.activity-2', '(4a)');
+    expect(fixture).toShow(Activity3Component, '(4b)');  // {Activity3Component} should still be showing
+    expect(fixture.debugElement.query(By.css('header > h1')).nativeElement.textContent).toEqual('Activity 3', '(4c)');
+
+    // Open 'activity-2' (guard allows activation)
+    Activity2CanActivate.CAN_ACTIVATE = true;
+    clickElement(fixture, ActivityPartComponent, 'a.activity-2', '(5a)');
+    expect(fixture).toShow(Activity2Component, '(5b)');
+    expect(fixture.debugElement.query(By.css('header>h1')).nativeElement.textContent).toEqual('Activity 2', '(5c)');
+
+    tick();
+  })));
+});
+
+/****************************************************************************************************
+ * Definition of App Test Module                                                                    *
+ ****************************************************************************************************/
+@Component({
+  template: `
+    <wb-workbench style="position: relative; width: 100%; height: 500px">
+      <wb-activity cssClass="activity-1" label="activity-1" title="Activity 1" routerLink="activity-1"></wb-activity>
+      <wb-activity cssClass="activity-2" label="activity-2" title="Activity 2" routerLink="activity-2"></wb-activity>
+      <wb-activity cssClass="activity-3" label="activity-3" title="Activity 3" routerLink="activity-3"></wb-activity>
+    </wb-workbench>
+  `
+})
+class AppComponent {
+}
+
+@Component({template: 'Activity-1'})
+class Activity1Component {
+}
+
+@Component({template: 'Activity-2'})
+class Activity2Component {
+}
+
+@Component({template: 'Activity-3'})
+class Activity3Component {
+}
+
+@Injectable()
+class Activity2CanActivate implements CanActivate {
+
+  public static CAN_ACTIVATE = false;
+
+  public canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return Activity2CanActivate.CAN_ACTIVATE;
+  }
+}
+
+@NgModule({
+  imports: [
+    WorkbenchModule.forRoot(),
+    NoopAnimationsModule,
+    CommonModule,
+    RouterTestingModule.withRoutes([
+      {path: 'activity-1', component: Activity1Component},
+      {path: 'activity-2', component: Activity2Component, canActivate: [Activity2CanActivate]},
+      {path: 'activity-3', component: Activity3Component},
+    ]),
+  ],
+  declarations: [
+    AppComponent,
+    Activity1Component,
+    Activity2Component,
+    Activity3Component
+  ],
+  providers: [
+    Activity2CanActivate
+  ]
+})
+class AppTestModule {
+}
+
