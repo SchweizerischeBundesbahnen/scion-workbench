aa32cc8fe4bfec8b874336eac6812eaf5e6e4062
Daniel Wiehl <daniel.wiehl@sbb.ch>
2019-03-17 00:51:30 +0100

    fix: show view tab title of inactive views when reloading the application
    fixes #121

--- projects/e2e/workbench/src/router.e2e-spec.ts
+++ projects/e2e/workbench/src/router.e2e-spec.ts
@@ -10,6 +10,7 @@
 
 import { AppPO } from './page-object/app.po';
 import { ViewNavigationPO } from './page-object/view-navigation.po';
+import { browser } from 'protractor';
 
 describe('Workbench Router', () => {
 
@@ -111,5 +112,78 @@ describe('Workbench Router', () => {
 
     await expect(viewNavigationPO.isActiveViewTab).toBeTruthy();
     await expect(appPO.getViewTabCount()).toEqual(1);
+  });
+
+  it('should show title of inactive views when reloading the application', async () => {
+    await viewNavigationPO.navigateTo();
+
+    // open view-1
+    await viewNavigationPO.enterPath('view');
+    await viewNavigationPO.enterMatrixParams({viewCssClass: 'e2e-view-1', viewTitle: 'view-1-title'});
+    await viewNavigationPO.navigate();
+
+    // open view-2
+    await viewNavigationPO.activateViewTab();
+    await viewNavigationPO.enterPath('view');
+    await viewNavigationPO.enterMatrixParams({viewCssClass: 'e2e-view-2', viewTitle: 'view-2-title'});
+    await viewNavigationPO.navigate();
+
+    // reload the application
+    await browser.refresh();
+
+    await expect(appPO.findViewTab('e2e-view-1').isActive()).toBeFalsy();
+    await expect(appPO.findViewTab('e2e-view-1').getTitle()).toEqual('view-1-title');
+
+    await expect(appPO.findViewTab('e2e-view-2').isActive()).toBeTruthy();
+    await expect(appPO.findViewTab('e2e-view-2').getTitle()).toEqual('view-2-title');
+
+    await expect((await browser.manage().logs().get('browser')).length).toEqual(0);
+  });
+
+  it('should not throw outlet activation error when opening a new view tab once a view tab was closed', async () => {
+    await browser.get('/');
+
+    // open view tab
+    await appPO.openNewViewTab();
+    await expect(appPO.getViewTabCount('e2e-welcome-page')).toEqual(1);
+
+    // close view tab
+    await appPO.findViewTab('e2e-welcome-page').close();
+    await expect(appPO.getViewTabCount('e2e-welcome-page')).toEqual(0);
+
+    // open view tab
+    await appPO.openNewViewTab();
+    await expect(appPO.getViewTabCount('e2e-welcome-page')).toEqual(1);
+    // expect no error to be thrown
+    await expect(browser.manage().logs().get('browser')).toEqual([]);
+
+    // open view tab
+    await appPO.openNewViewTab();
+    await expect(appPO.getViewTabCount('e2e-welcome-page')).toEqual(2);
+    // expect no error to be thrown
+    await expect(browser.manage().logs().get('browser')).toEqual([]);
+  });
+
+  it('should close all views in a row', async () => {
+    await browser.get('/');
+
+    // open multiple view tabs
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+    await appPO.openNewViewTab();
+
+    // close all view tabs
+    await appPO.closeAllViewTabs();
+
+    await expect(appPO.getViewTabCount()).toEqual(0);
+
+    // expect no error to be thrown
+    await expect(browser.manage().logs().get('browser')).toEqual([]);
   });
 });
