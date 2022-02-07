294f1b29cd72906af7b791b9a959b661c9d81f3a
danielwiehl <daniel.wiehl@yahoo.com>
2022-02-07 14:04:23 +0100

    chore: update copyright headers

--- projects/scion/e2e-testing/src/workbench-client/page-object/popup-opener-page.po.ts
+++ projects/scion/e2e-testing/src/workbench-client/page-object/popup-opener-page.po.ts
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2018-2019 Swiss Federal Railways
+ * Copyright (c) 2018-2022 Swiss Federal Railways
  *
  * This program and the accompanying materials are made
  * available under the terms from the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
- *  SPDX-License-Identifier: EPL-2.0
+ * SPDX-License-Identifier: EPL-2.0
  */
 
 import {assertPageToDisplay, enterText, selectOption} from '../../helper/testing.util';
