d469de7df9a2c829adf874faa328a7d7028f8f4d
k-genov <konstantin.genov@gmail.com>
2024-06-11 11:06:35 +0200

    deps(workbench-testing-app): use latest version of SCION Microfrontend Platform DevTools

--- apps/workbench-testing-app/src/environments/environment.ts
+++ apps/workbench-testing-app/src/environments/environment.ts
@@ -31,7 +31,7 @@ const microfrontendPlatformConfig: MicrofrontendPlatformConfig = {
   applications: [
     {symbolicName: 'workbench-client-testing-app1', manifestUrl: 'http://localhost:4201/assets/manifest-app1.json', intentionRegisterApiDisabled: false},
     {symbolicName: 'workbench-client-testing-app2', manifestUrl: 'http://localhost:4202/assets/manifest-app2.json', intentionRegisterApiDisabled: false},
-    {symbolicName: 'devtools', manifestUrl: 'https://scion-microfrontend-platform-devtools-v1-2-2.vercel.app/assets/manifest.json', intentionCheckDisabled: true, scopeCheckDisabled: true},
+    {symbolicName: 'devtools', manifestUrl: 'https://scion-microfrontend-platform-devtools-v1-3-0.vercel.app/assets/manifest.json', intentionCheckDisabled: true, scopeCheckDisabled: true},
   ],
 };
 
