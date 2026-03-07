5b0972dc6e3230d16ffe0e6d42c1c5a531cc8e90
danielwiehl <daniel.wiehl@yahoo.com>
2026-03-07 23:32:24 +0100

    remove unused functions

--- projects/scion/sci-components/menu/src/menu-contribution.ts
+++ projects/scion/sci-components/menu/src/menu-contribution.ts
@@ -1,8 +1,6 @@
 import {SciMenu, SciMenuGroup} from './menu/menu.model';
-import {ɵSciMenu} from './menu/ɵmenu.model';
 import {assertNotInReactiveContext, effect, inject, Injector, untracked} from '@angular/core';
 import {SciToolbar, SciToolbarGroup} from './toolbar/toolbar.model';
-import {ɵSciToolbar} from './toolbar/ɵtoolbar.model';
 import {Disposable} from './common/disposable';
 import {ɵSciMenuService} from './ɵmenu.service';
 import {SciMenuContextProvider} from './menu-context-provider';
@@ -42,57 +40,6 @@ export function contributeMenu(locationLike: string | SciContributionLocation, f
   return {
     dispose: () => injector.destroy(),
   };
-}
-
-// export function contributeMenu(locationLike: string | SciContributionLocation, factoryFn: Function, options?: SciMenuContributionOptions): Disposable {
-//   const injector = Injector.create({parent: options?.injector ?? inject(Injector), providers: []});
-//
-//   const {location, before, after, context} = typeof locationLike === 'string' ? {location: locationLike} as SciContributionLocation : locationLike;
-//   return runInInjectionContext(injector, () => {
-//     const menuService = inject(ɵSciMenuService);
-//
-//     const {contributions, menuContributions, groupContributions} = constructMenu(location, factoryFn);
-//     const environmentMenuContext = coerceSignal(inject(SciMenuContextProvider, {optional: true})?.provideContext?.());
-//
-//     // Sort relative to other contributions.
-//     contributions.forEach(contribution => contribution.position = {before, after});
-//
-//     effect((onCleanup) => {
-//       const mergedContext = new Map([...environmentMenuContext?.() ?? new Map(), ...context ?? new Map()]);
-//
-//       untracked(() => {
-//         const registrations = new Array<Disposable>();
-//         registrations.push(menuService.contributeMenu(location, contributions, mergedContext));
-//         registrations.push(...menuContributions.map(menuContribution => contributeMenu({location: menuContribution.location, context}, menuContribution.factoryFn, {...options, injector})));
-//         registrations.push(...groupContributions.map(groupContribution => contributeMenu({location: groupContribution.location, context}, groupContribution.factoryFn, {...options, injector})));
-//
-//         onCleanup(() => {
-//           registrations.forEach(registration => registration.dispose());
-//           registrations.length = 0;
-//         })
-//       });
-//     });
-//
-//     return {
-//       dispose: () => injector.destroy(),
-//     };
-//   });
-// }
-
-function constructMenu(location: string, factoryFn: Function): ɵSciMenu | ɵSciToolbar {
-  if (location.startsWith('menu:') || location.startsWith('group(menu):')) {
-    const menu = new ɵSciMenu();
-    factoryFn(menu);
-    return menu;
-  }
-  else if (location.startsWith('toolbar:') || location.startsWith('group(toolbar):')) {
-    const toolbar = new ɵSciToolbar();
-    factoryFn(toolbar);
-    return toolbar;
-  }
-  else {
-    throw Error('Illegal contribution point');
-  }
 }
 
 function normalizeLocation(location: `menu:${string}` | `toolbar:${string}` | `group(menu):${string}` | `group(toolbar):${string}`): `menu:${string}` | `toolbar:${string}` | `group:${string}` {
